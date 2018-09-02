6274a5fde8be99ab073df2cf9180be55c7bfc08e
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-02 18:59:53 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,14 +4,10 @@ class Show < ActiveRecord::Base
   #belongs_to :genre
   belongs_to :network
 
-  def array_of_characters
-    character_array = []
-    self.shows.characters.map do |character|
-        character_array << character.name
-    end
-  end
-
-
-
-
+  #def array_of_characters
+  #  character_array = []
+  #  self.shows.characters.map do |character|
+    #    character_array << character.name
+    #end
+#  end
 end
